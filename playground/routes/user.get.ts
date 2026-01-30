37c88f38c605c43edf2c9cad0a3342dd43da7383
schplitt <schplitt.contact@gmail.com>
2026-01-30 13:00:57 +0100

    fix: playground

--- playground/routes/user.get.ts
+++ playground/routes/user.get.ts
@@ -1,8 +1,8 @@
 import { defineEventHandler } from 'nitro/h3'
 import { useUserClient } from 'c8y-nitro/utils'
 
-export default defineEventHandler(async () => {
-  const client = useUserClient()
+export default defineEventHandler(async (event) => {
+  const client = useUserClient(event)
   const res = await client.user.current()
   const user = res.data
   return user
