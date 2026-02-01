33ab9c9667a1e23c142486762e92fca86b0f61db
schplitt <schplitt.contact@gmail.com>
2026-02-01 20:31:49 +0100

    docs: update comments in runtime.d.ts for clarity and context

--- src/runtime.d.ts
+++ src/runtime.d.ts
@@ -1,3 +1,7 @@
+// Files used to emulate imported runtime values in Nitro runtime
+// Necessary if virtual values are used by e.g. utils
+// Should match the generated types in module/runtime.ts
+
 declare module 'c8y-nitro/runtime' {
   import type { C8YRoles } from 'c8y-nitro/types'
 
