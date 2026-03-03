8319313b49b3c39f27c5d15756194dddb402ebe9
schplitt <schplitt.contact@gmail.com>
2026-03-03 07:45:42 +0100

    chore: add logging example to playground

--- playground/routes/user.get.ts
+++ playground/routes/user.get.ts
@@ -1,9 +1,12 @@
 import { defineEventHandler } from 'nitro/h3'
 import { useUserClient } from 'c8y-nitro/utils'
+import { useLogger } from 'evlog'
 
 export default defineEventHandler(async (event) => {
   const client = useUserClient(event)
+  const log = useLogger(event)
   const res = await client.user.current()
   const user = res.data
+  log.set({ action: 'get-user', user: { user: user.userName } })
   return user
 })
